baf5c5236e54227ab7a39e3745ba6653b1e78a0c
cipron81 <cipron81@users.noreply.github.com>
2016-10-14 13:05:33 +0900

    changed

--- default_setting.sh
+++ default_setting.sh
@@ -11,4 +11,8 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 # http://loumo.jp/wp/archive/20131217000405/
 find . -name .localized -delete
 
+# http://inforati.jp/apple/mac-tips-techniques/system-hints/how-to-display-the-full-path-of-folder-in-macos-finder-titlebar.html
+defaults write com.apple.finder _FXShowPosixPathInTitle -boolean true
+
+
 killall Finder
